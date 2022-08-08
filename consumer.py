1c4858406cbc8e1a61c979cf7307d9e5247715f2
Isha Talegaonkar <isha.talegaonkar@gmail.com>
2022-08-08 08:33:52 -0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,16 +1,16 @@
 from kafka import KafkaConsumer
-from json import loads
+import json 
 import time
 
 consumer = KafkaConsumer(
     'jobs',
-     bootstrap_servers=['localhost:9092'],
+     bootstrap_servers=['127.0.0.1:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
-     group_id='my-group',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
+     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
 
 for message in consumer:
-    message = message.value
+    jobs = message.value
     time.sleep(5)
-    print('{} added'.format(message))
+    print('{} added'.format(jobs))
